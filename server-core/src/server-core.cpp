de53f2a16e7302ffce14500caaf71be2a301f86d
Martin Alebachew <martinalebachew@gmail.com>
2023-04-14 10:47:25 +0300

    Set up CMake
    - Renamed main file.
    - Configured CMake.
    - Updated .gitignore.

--- server-core/src/server-core.cpp
+++ server-core/src/server-core.cpp
@@ -1,6 +1,8 @@
 // main.cpp
 // (C) Martin Alebachew, 2023
 
+#include <iostream>
+
 int main(int argc, const char * argv[]) {
     std::cout << "Lighthouse Server Core" << std::endl;
     std::cout << "By Martin Alebachew\n" << std::endl;
