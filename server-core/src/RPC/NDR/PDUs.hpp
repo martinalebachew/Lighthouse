1a30ec5be6f85c54763e7da3dc4d06f87512ef38
Martin Alebachew <martinalebachew@gmail.com>
2023-04-22 23:35:34 +0300

    Added PDU Types
    - Added PDU Types Enum.

---
+++ server-core/src/RPC/NDR/PDUs.hpp
@@ -0,0 +1,28 @@
+// PDUs.hpp
+// (C) Martin Alebachew, 2023
+
+#pragma once
+#include "primitives.hpp"
+
+// PDU Types
+enum PDU : ENUM {
+    request,
+    ping,
+    response,
+    fault,
+    working,
+    nocall,
+    reject,
+    ack,
+    cl_cancel,
+    fack,
+    cancel_ack,
+    bind,
+    bind_ack,
+    bind_nak,
+    alter_context,
+    alter_context_resp,
+    shutdown = 17, // Skipping 16 as defined in C706
+    co_cancel,
+    orphaned
+};
