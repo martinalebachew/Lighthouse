08aa5d0348c5efc4dc9c00361e29105ae4258bec
Martin Alebachew <martinalebachew@gmail.com>
2023-04-23 00:19:36 +0300

    Created Bind PDU struct
    - Created Bind PDU struct.
    - Added file description.

--- core/src/rpc/ndr/pdus.hpp
+++ core/src/rpc/ndr/pdus.hpp
@@ -1,7 +1,13 @@
 // pdus.hpp
 // (C) Martin Alebachew, 2023
 
+/*
+This file contains PDU structure definitions and
+underlaying data types that are directly relevant.
+*/
+
 #pragma once
+#include "presentation.hpp"
 #include "primitives.hpp"
 
 enum struct PDU : u_int8 {
@@ -24,4 +30,22 @@ enum struct PDU : u_int8 {
   shutdown = 17, // Skipping 16 as defined in C706
   co_cancel,
   orphaned
+};
+
+struct bind_pdu_t {
+  u_int8 rpc_vers = 5;
+  u_int8 rpc_vers_minor = 0;
+  PDU_Types PTYPE = PDU_Types::bind;
+  u_int8 pfc_flags;
+  byte packed_drep[4]; // NDR data rep format label
+  u_int16 frag_length;
+  u_int16 auth_length = 0;
+  u_int32 call_id;
+
+  u_int16 max_xmit_frag;  // Max transmit frag size, bytes
+  u_int16 max_recv_frag;  // Max receive frag size, bytes
+  u_int32 assoc_group_id; // Incarnation of client-server assoc group
+
+  // Presentation context list, variable size
+  p_cont_list_t p_context_elem;
 };
