b32fe8e7be0c3c3a957ef7c664d60eef8d4a6226
Martin Alebachew <martinalebachew@gmail.com>
2023-04-23 12:40:30 +0300

    Added socket shared pointer
    - Added socket shared pointer so it could be easily passed to functions.

--- server-core/src/server-core.cpp
+++ server-core/src/server-core.cpp
@@ -7,8 +7,7 @@
 using boost::asio::ip::tcp;
 #define PORT 1688 // Default KMS host port
 
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
     std::cout << "Lighthouse Server Core\n"
               << "By Martin Alebachew\n"
               << std::endl;
@@ -17,15 +16,14 @@ int main(int argc, const char *argv[])
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
     std::cout << "[TCP] Listening on port " << PORT << std::endl;
 
-    while (true) // TODO: replace iterative server with asynchronous server
-    {
+    while (true) { // TODO: replace iterative server with asynchronous server
         // 1. Establish TCP connection with client
-        tcp::socket socket(io_context);
+        std::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr = std::make_shared<boost::asio::ip::tcp::socket>(io_context);
         tcp::endpoint peer_endpoint;
-        acceptor.accept(socket, peer_endpoint);
+        acceptor.accept(*socket_ptr, peer_endpoint);
         std::cout << "[TCP] Accepted connection from " << peer_endpoint << std::endl;
 
-        socket.close();
+        socket_ptr->close();
         std::cout << "[TCP] Closed connection from " << peer_endpoint << std::endl;
     }
 
