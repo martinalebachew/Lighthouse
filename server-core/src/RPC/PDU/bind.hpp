f4f92cb398a20dc269d364c9950881cf937aefca
Martin Alebachew <martinalebachew@gmail.com>
2023-05-01 22:34:27 +0300

    Completed Bind PDU constructor
    - Completed Bind PDU constructor that parses raw PDU into the Bind PDU.
    - Disabled compiler alignment of RPC data structures.

--- server-core/src/RPC/PDU/bind.hpp
+++ server-core/src/RPC/PDU/bind.hpp
@@ -7,12 +7,18 @@ This files defines the RPC bind PDU wrapper class.
 
 #pragma once
 #include <cstring>
+#include <cstdlib>
 #include "../primitives.hpp"
 #include "../presentation.hpp"
 #include "type.hpp"
 
 namespace RPC::PDU {
 struct Bind {
+    /* NOTE
+     Modifying the properties below may break the casting algorithm.
+     Adjust it accordingly.
+    */
+
     /* common fields */
     u_int8 rpc_vers = 5; /* 00:01 RPC version */
     u_int8 rpc_vers_minor = 0; /* 01:01 minor version */
@@ -31,13 +37,48 @@ struct Bind {
     /* presentation context list */
     p_cont_list_t p_context_elem; /* variable size */
 
-    inline Bind() { }
-
     inline Bind(const std::vector<char>& rawPDU) {
-        // Copy flat data
-        memcpy(&pfc_flags, rawPDU.data() + offsetof(Bind, pfc_flags), offsetof(Bind, p_context_elem) - offsetof(Bind, pfc_flags));
+        // Copy PDU fixed-size properties, including those nested in p_cont_list_t type.
+        memcpy(this, // Copy into the beginning of this structure
+               rawPDU.data(), // Copy from the beginning of the raw PDU
+               offsetof(Bind, p_context_elem) + offsetof(p_cont_list_t, p_cont_elem)
+               // Copy all fixed-size properties, including those nested in p_cont_list_t type.
+               );
 
-        // TODO: Copy complex data types to heap and save pointer, account for it in the destructor
+        // Allocate array of p_cont_list_t->p_cont_elem_t on the heap
+        p_context_elem.p_cont_elem = (p_cont_elem_t*)malloc(p_context_elem.n_context_elem * sizeof(p_cont_elem_t));
+
+        // Iterate through p_cont_list_t->p_cont_elem_t elements
+        p_cont_elem_t* CurrContElemPtr = (p_cont_elem_t*)(rawPDU.data() + offsetof(Bind, p_context_elem) + offsetof(p_cont_list_t, p_cont_elem));
+        for (int i = 0; i < p_context_elem.n_context_elem; i++) {
+            memcpy((void*)(&p_context_elem.p_cont_elem[i]), // Copy into the heap-allocated array
+                   CurrContElemPtr, // Copy from the calculated offset in the raw PDU
+                   offsetof(p_cont_elem_t, transfer_syntaxes) // Copy all fixed-size properties
+                   );
+
+            // Allocate array of p_cont_list_t->p_cont_elem_t->p_syntax_id_t on the heap
+            p_context_elem.p_cont_elem[i].transfer_syntaxes = (p_syntax_id_t*)malloc(p_context_elem.p_cont_elem[i].n_transfer_syn * sizeof(p_syntax_id_t));
+
+            // Iterate through p_cont_list_t->p_cont_elem_t->p_syntax_id_t elements
+            p_syntax_id_t* CurrSyntaxIdPtr = (p_syntax_id_t*)((byte*)CurrContElemPtr + offsetof(p_cont_elem_t, transfer_syntaxes)); // Pointer to the transfer_syntaxes data
+            for (int j = 0; j < p_context_elem.p_cont_elem[i].n_transfer_syn; j++) {
+                memcpy((void*)(&p_context_elem.p_cont_elem[i].transfer_syntaxes[j]), // Copy into the heap-allocated array
+                       CurrSyntaxIdPtr, // Copy from the calculated offset in the raw PDU
+                       sizeof(p_syntax_id_t) // Copy all properties
+                       );
+
+                CurrSyntaxIdPtr += 1; // Advance the pointer to the next p_syntax_id_t
+            }
+
+            // Set the next p_cont_list_t->p_cont_elem_t element
+            CurrContElemPtr = (p_cont_elem_t*)CurrSyntaxIdPtr;
+        }
     }
-};
+
+    ~Bind() {
+        for (int i = 0; i < p_context_elem.n_context_elem; i++)
+            free(p_context_elem.p_cont_elem[i].transfer_syntaxes);
+        free(p_context_elem.p_cont_elem);
+    }
+} __attribute__ ((packed)); // Disabling compiler alignment in favor of RPC alignment.
 } // namespace RPC::PDU
--- server-core/src/RPC/presentation.hpp
+++ server-core/src/RPC/presentation.hpp
@@ -12,22 +12,22 @@ for presentation context.
 
 typedef u_int16 p_context_id_t;
 
-typedef struct {
+struct p_syntax_id_t {
     dce_uuid_t if_uuid;
     u_int32 if_version;
-} p_syntax_id_t;
+} __attribute__ ((packed)); // Disabling compiler alignment in favor of RPC alignment.;
 
-typedef struct {
+struct p_cont_elem_t {
     p_context_id_t p_cont_id;
     u_int8 n_transfer_syn; /* number of items */
     u_int8 reserved; /* alignment pad, m.b.z. */
     p_syntax_id_t abstract_syntax; /* transfer syntax list */
-    p_syntax_id_t transfer_syntaxes[];
-} p_cont_elem_t;
+    p_syntax_id_t* transfer_syntaxes; // Heap-allocated array
+} __attribute__ ((packed)); // Disabling compiler alignment in favor of RPC alignment.
 
-typedef struct {
+struct p_cont_list_t {
     u_int8 n_context_elem; /* number of items */
     u_int8 reserved; /* alignment pad, m.b.z. */
     u_int16 reserved2; /* alignment pad, m.b.z. */
-    p_cont_elem_t p_cont_elem[];
-} p_cont_list_t;
+    p_cont_elem_t* p_cont_elem; // Heap-allocated array
+} __attribute__ ((packed)); // Disabling compiler alignment in favor of RPC alignment.
