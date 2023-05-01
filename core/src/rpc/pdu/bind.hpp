a01c48aa1726e4cd926a906b532edeea2e35e4d9
Martin Alebachew <martinalebachew@gmail.com>
2023-05-01 22:34:27 +0300

    Completed Bind PDU constructor
    - Completed Bind PDU constructor that parses raw PDU into Bind PDU.
    - Implemented matching destructor.
    - Disabled compiler alignment of RPC data structures.

--- core/src/rpc/pdu/bind.hpp
+++ core/src/rpc/pdu/bind.hpp
@@ -9,11 +9,17 @@ This file defines the RPC Bind PDU struct.
 #include "../presentation.hpp"
 #include "../primitives.hpp"
 #include "types.hpp"
+#include <cstdlib>
 #include <cstring>
 #include <vector>
 
 namespace RPC::PDU {
 struct Bind {
+  /* NOTE
+  Modifying the properties below may break the casting algorithm.
+  Adjust it accordingly.
+  */
+
   u_int8 rpc_vers = 5;
   u_int8 rpc_vers_minor = 0;
   Type PTYPE = Type::bind;
@@ -31,13 +37,56 @@ struct Bind {
   p_cont_list_t p_context_elem;
 
   Bind(const std::vector<byte> &rawPDU) {
-    // Copy flat data
+    // Copy PDU fixed-size properties, including those nested in p_cont_list_t type
     memcpy(
-      &pfc_flags, rawPDU.data() + offsetof(Bind, pfc_flags),
-      offsetof(Bind, p_context_elem) - offsetof(Bind, pfc_flags)
+      this, rawPDU.data(),
+      offsetof(Bind, p_context_elem) + offsetof(p_cont_list_t, p_cont_elem)
     );
 
-    // TODO: Copy complex data types to heap and save pointer, account for it in the destructor
+    // Allocate array of p_cont_list_t->p_cont_elem_t on the heap
+    p_context_elem.p_cont_elem =
+      (p_cont_elem_t*)malloc(p_context_elem.n_context_elem * sizeof(p_cont_elem_t));
+
+    // Iterate through p_cont_list_t->p_cont_elem_t elements
+    p_cont_elem_t* CurrContElemPtr =
+      (p_cont_elem_t*)(rawPDU.data() + offsetof(Bind, p_context_elem) +
+                       offsetof(p_cont_list_t, p_cont_elem));
+
+    for (int i = 0; i < p_context_elem.n_context_elem; i++) {
+      // Copy all fixed-size properties from the raw PDU into the heap-allocated array
+      memcpy(
+        (void*)(&p_context_elem.p_cont_elem[i]), CurrContElemPtr,
+        offsetof(p_cont_elem_t, transfer_syntaxes)
+      );
+
+      // Allocate array of p_cont_list_t->p_cont_elem_t->p_syntax_id_t on the heap
+      p_context_elem.p_cont_elem[i].transfer_syntaxes = (p_syntax_id_t*)
+        malloc(p_context_elem.p_cont_elem[i].n_transfer_syn * sizeof(p_syntax_id_t));
+
+      // Iterate through p_cont_list_t->p_cont_elem_t->p_syntax_id_t elements
+      // Pointer to the transfer_syntaxes data
+      p_syntax_id_t* CurrSyntaxIdPtr =
+        (p_syntax_id_t*)((byte*)CurrContElemPtr + offsetof(p_cont_elem_t, transfer_syntaxes));
+
+      for (int j = 0; j < p_context_elem.p_cont_elem[i].n_transfer_syn; j++) {
+        // Copy all properties from the raw PDU into the heap-allocated array
+        memcpy(
+          (void*)(&p_context_elem.p_cont_elem[i].transfer_syntaxes[j]),
+          CurrSyntaxIdPtr, sizeof(p_syntax_id_t)
+        );
+
+        CurrSyntaxIdPtr += 1; // Advance the pointer to the next p_syntax_id_t
+      }
+
+      // Set the next p_cont_list_t->p_cont_elem_t element
+      CurrContElemPtr = (p_cont_elem_t*)CurrSyntaxIdPtr;
+    }
   }
-};
+
+  ~Bind() {
+    for (int i = 0; i < p_context_elem.n_context_elem; i++)
+      free(p_context_elem.p_cont_elem[i].transfer_syntaxes);
+    free(p_context_elem.p_cont_elem);
+  }
+} __attribute__((packed)); // Disabling compiler alignment in favor of RPC alignment
 } // namespace RPC::PDU
--- core/src/rpc/presentation.hpp
+++ core/src/rpc/presentation.hpp
@@ -15,7 +15,7 @@ typedef u_int16 p_context_id_t;
 struct p_syntax_id_t {
   dce_uuid_t if_uuid;
   u_int32 if_version;
-};
+} __attribute__((packed)); // Disabling compiler alignment in favor of RPC alignment
 
 struct p_cont_elem_t {
   p_context_id_t p_cont_id;
@@ -23,11 +23,11 @@ struct p_cont_elem_t {
   u_int8 reserved;
   p_syntax_id_t abstract_syntax;
   p_syntax_id_t* transfer_syntaxes; // Heap-allocated array
-};
+} __attribute__((packed));          // Disabling compiler alignment in favor of RPC alignment
 
 struct p_cont_list_t {
   u_int8 n_context_elem; // Number of items
   u_int8 reserved;
   u_int16 reserved2;
   p_cont_elem_t* p_cont_elem; // Heap-allocated array
-};
+} __attribute__((packed));    // Disabling compiler alignment in favor of RPC alignment
