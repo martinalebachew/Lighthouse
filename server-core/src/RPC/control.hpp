66af5880417c9736c2f349fc22ad2f9c2395e2f3
Martin Alebachew <martinalebachew@gmail.com>
2023-05-02 10:21:40 +0300

    Moved conversation class
    - Moved conversation class into header file.

--- server-core/src/RPC/control.hpp
+++ server-core/src/RPC/control.hpp
@@ -1,17 +1,33 @@
-// control.cpp
+// control.hpp
 // (C) Martin Alebachew, 2023
 
-#pragma once
-#include <boost/asio.hpp>
-#include <iostream>
-#include <vector>
-#include "presentation.hpp"
+/*
+This file contains RPC conversation control
+wrapper.
+*/
+
 #include "primitives.hpp"
+#define HEAD_SIZE 10 // Frag length is specified in 9-10 bytes of CO PDUs (index 8-9)
 
 namespace RPC::Control {
-    class Conversation {
-    public:
-        std::vector<byte> ReceiveFragment(boost::asio::ip::tcp::socket &socket);
-        std::vector<byte> ReceivePDU(boost::asio::ip::tcp::socket &socket);
+    struct Conversation {
+        std::vector<byte> inline ReceiveFragment(boost::asio::ip::tcp::socket &socket) {
+            std::vector<byte> PDU(HEAD_SIZE);
+            boost::asio::read(socket, boost::asio::buffer(PDU));
+
+            // TODO: Account for endianness specified in format label and for host endianness This only handles little endian to little endian
+            size_t size = *(u_int16 *) (&PDU.at(HEAD_SIZE - 2));
+            PDU.resize(size);
+            std::cout << "[RPC] PDU Fragment Size: " << size << " bytes" << std::endl;
+
+            boost::asio::read(socket, boost::asio::buffer(PDU.data() + HEAD_SIZE * sizeof(PDU[0]),
+                                                        size - HEAD_SIZE)); // Receive rest of the PDU
+            return PDU;
+        };
+
+        std::vector<byte> inline ReceivePDU(boost::asio::ip::tcp::socket &socket) {
+            // No support for fragmented PDUs
+            return ReceiveFragment(socket); // TODO: Throw error if PDU is fragmented
+        }
     };
-} // namespace RPC::Control
+}
