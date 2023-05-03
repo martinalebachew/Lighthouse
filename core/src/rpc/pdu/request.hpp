c7e83c413d2a8932d73943f3ae4afff5cd9572c4
Martin Alebachew <martinalebachew@gmail.com>
2023-05-03 16:15:49 +0300

    Implemented Request PDU constructor
    - Implemented Request PDU constructor that parses raw PDU into Request PDU.
    - Implemented matching destructor.

--- core/src/rpc/pdu/request.hpp
+++ core/src/rpc/pdu/request.hpp
@@ -38,5 +38,32 @@ struct Request {
   dce_uuid_t object_uuid;
 
   byte* stub_data;
+
+  Request(const std::vector<byte> &rawPDU) {
+    // Copy PDU fixed-size properties
+    memcpy(this, rawPDU.data(), offsetof(Request, object_uuid));
+
+    // Calculate stub_data length
+    unsigned int stub_data_length = rawPDU.size() - offsetof(Request, object_uuid);
+
+    // Set object_uuid property
+    if (pfc_flags & PFC_OBJECT_UUID) {
+      // Copy object_uuid from the raw PDU
+      memcpy(
+        &object_uuid, rawPDU.data() + offsetof(Request, object_uuid), sizeof(object_uuid)
+      );
+
+      // Subtract object_uuid length, as it is present in the raw PDU
+      stub_data_length -= sizeof(object_uuid);
+    }
+
+    // Allocate and copy stub_data from the raw PDU
+    stub_data = (byte*)malloc(stub_data_length);
+    memcpy(stub_data, rawPDU.data() + rawPDU.size() - stub_data_length, stub_data_length);
+  }
+
+  ~Request() {
+    free(stub_data);
+  }
 } __attribute__((packed)); // Disabling compiler alignment in favor of RPC alignment
 } // namespace RPC::PDU
