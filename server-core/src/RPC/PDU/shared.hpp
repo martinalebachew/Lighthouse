fd53a0c21ded23318049264f84f268d84635ec25
Restyled.io <commits@restyled.io>
2023-05-04 06:57:10 +0000

    Restyled by clang-format

--- server-core/src/RPC/PDU/shared.hpp
+++ server-core/src/RPC/PDU/shared.hpp
@@ -27,24 +27,24 @@ This file defines RPC PDU shared types.
 namespace RPC::PDU {
 // PDU Types
 enum struct Type : u_int8 {
-    request,
-    ping,
-    response,
-    fault,
-    working,
-    nocall,
-    reject,
-    ack,
-    cl_cancel,
-    fack,
-    cancel_ack,
-    bind,
-    bind_ack,
-    bind_nak,
-    alter_context,
-    alter_context_resp,
-    shutdown = 17, // Skipping 16 as defined in C706
-    co_cancel,
-    orphaned
+  request,
+  ping,
+  response,
+  fault,
+  working,
+  nocall,
+  reject,
+  ack,
+  cl_cancel,
+  fack,
+  cancel_ack,
+  bind,
+  bind_ack,
+  bind_nak,
+  alter_context,
+  alter_context_resp,
+  shutdown = 17, // Skipping 16 as defined in C706
+  co_cancel,
+  orphaned
 };
 }; // namespace RPC::PDU
--- server-core/src/server-core.cpp
+++ server-core/src/server-core.cpp
@@ -8,26 +8,26 @@ using boost::asio::ip::tcp;
 #define PORT 1688 // Default KMS host port
 
 int main(int argc, const char *argv[]) {
-    std::cout << "Lighthouse Server Core\n"
-              << "By Martin Alebachew\n"
+  std::cout << "Lighthouse Server Core\n"
+            << "By Martin Alebachew\n"
+            << std::endl;
+
+  boost::asio::io_context io_context;
+  tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
+  std::cout << "[TCP] Listening on port " << PORT << std::endl;
+
+  while (true) { // TODO: replace iterative server with asynchronous server
+    // 1. Establish TCP connection with client
+    std::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr =
+        std::make_shared<boost::asio::ip::tcp::socket>(io_context);
+    tcp::endpoint peer_endpoint;
+    acceptor.accept(*socket_ptr, peer_endpoint);
+    std::cout << "[TCP] Accepted connection from " << peer_endpoint
               << std::endl;
 
-    boost::asio::io_context io_context;
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
-    std::cout << "[TCP] Listening on port " << PORT << std::endl;
+    socket_ptr->close();
+    std::cout << "[TCP] Closed connection from " << peer_endpoint << std::endl;
+  }
 
-    while (true) { // TODO: replace iterative server with asynchronous server
-        // 1. Establish TCP connection with client
-        std::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr =
-            std::make_shared<boost::asio::ip::tcp::socket>(io_context);
-        tcp::endpoint peer_endpoint;
-        acceptor.accept(*socket_ptr, peer_endpoint);
-        std::cout << "[TCP] Accepted connection from " << peer_endpoint
-                  << std::endl;
-
-        socket_ptr->close();
-        std::cout << "[TCP] Closed connection from " << peer_endpoint << std::endl;
-    }
-
-    return 0;
+  return 0;
 }
