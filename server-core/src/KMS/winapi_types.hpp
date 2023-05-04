4406cd260155e3584ca970f1620698843b0d4540
Martin Alebachew <martinalebachew@gmail.com>
2023-05-04 17:09:42 +0300

    Added WinAPI types
    - Added WinAPI types required for KMS.

---
+++ server-core/src/KMS/winapi_types.hpp
@@ -0,0 +1,21 @@
+// winapi_types.hpp
+// (C) Martin Alebachew, 2023
+
+/*
+This file defines the WinAPI types required for KMS.
+*/
+
+#pragma once
+#include "../primitives.hpp"
+
+typedef unsigned short WORD;
+typedef unsigned long  DWORD;
+
+typedef struct _GUID {
+  unsigned long  Data1;
+  unsigned short Data2;
+  unsigned short Data3;
+  unsigned char  Data4[8];
+} GUID;
+
+typedef wchar_t WCHAR;
