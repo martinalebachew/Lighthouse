d29f79950f50924d64a4878126e3ff2dd21500c1
Martin Alebachew <martinalebachew@gmail.com>
2023-05-05 20:42:25 +0300

    Created Response PDU
    - Created Response PDU and declared PDU properties.

---
+++ server-core/src/RPC/PDU/response.hpp
@@ -0,0 +1,43 @@
+// response.hpp
+// (C) Martin Alebachew, 2023
+
+/*
+This files defines the RPC response PDU wrapper struct.
+*/
+
+#pragma once
+#include "request.hpp"
+#include "shared.hpp"
+#include "../primitives.hpp"
+#include "../uuid.hpp"
+#include "shared.hpp"
+#include <cstdlib>
+#include <cstring>
+
+namespace RPC::PDU {
+struct Response {
+  /* NOTE
+     Modifying the properties below may break the casting algorithm.
+     Adjust it accordingly.
+  */
+
+  /* common fields */
+  u_int8 rpc_vers = 5;                               /* RPC version */
+  u_int8 rpc_vers_minor = 0;                         /* minor version */
+  Type PTYPE = Type::response;                       /* bind PDU */
+  u_int8 pfc_flags = PFC_FIRST_FRAG | PFC_LAST_FRAG; /* flags */
+  byte packed_drep[4];     /* NDR data rep format label */
+  u_int16 frag_length;     /* total length of fragment */
+  u_int16 auth_length = 0; /* length of auth_value */
+  u_int32 call_id;         /* call identifier */
+  /* end common fields */
+
+  u_int32 alloc_hint; /* allocation hint */
+  p_context_id_t p_cont_id; /* pres context, i.e. data rep */
+  u_int8 cancel_count; /* cancel count */
+  u_int8 reserved; /* reserved, m.b.z. */
+
+  std::vector<byte> stub;
+} __attribute__((
+    packed)); // Disabling compiler alignment in favor of RPC alignment.
+} // namespace RPC::PDU
