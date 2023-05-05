cb3e67112c1999b46d3c93d0f721503571bb793d
Martin Alebachew <martinalebachew@gmail.com>
2023-05-05 21:16:16 +0300

    Implemented Response PDU toBuffer()
    - Implemented Response PDU toBuffer() method that returns a bytes vector ready for sending.

--- server-core/src/RPC/PDU/response.hpp
+++ server-core/src/RPC/PDU/response.hpp
@@ -48,6 +48,16 @@ struct Response {
     call_id = request.call_id; // This value is set by the client  
     stub = responseStub;
   }
+
+  std::vector<byte> toBuffer() {
+    frag_length = offsetof(Response, stub) + stub.size();
+    alloc_hint = frag_length; // Irrelevant since fragmented requests are not implemented
+
+    std::vector<byte> buffer = std::vector<byte>(frag_length);
+    memcpy(buffer.data(), this, offsetof(Response, stub));
+    memcpy(buffer.data() + offsetof(Response, stub), stub.data(), stub.size());
+    return buffer;
+  }
 } __attribute__((
     packed)); // Disabling compiler alignment in favor of RPC alignment.
 } // namespace RPC::PDU
