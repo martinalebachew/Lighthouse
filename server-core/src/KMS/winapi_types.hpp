d3da1b2e9193ba34269b5a4acdd10f039c3099df
Martin Alebachew <martinalebachew@gmail.com>
2023-05-12 12:51:13 +0300

    Implemented GUID::toString()
    - Implemented GUID::toString() method.

--- server-core/src/KMS/winapi_types.hpp
+++ server-core/src/KMS/winapi_types.hpp
@@ -19,6 +19,17 @@ struct GUID {
   u_int16_t Data2;
   u_int16_t Data3;
   byte      Data4[8];
+
+  std::string toString() {
+    char guid_cstr[37];
+    snprintf(guid_cstr, sizeof(guid_cstr),
+            "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+            Data1, Data2, Data3,
+            Data4[0], Data4[1], Data4[2], Data4[3],
+            Data4[4], Data4[5], Data4[6], Data4[7]);
+
+    return std::string(guid_cstr);
+  }
 } __attribute__((
     packed)); // Disabling compiler alignment in favor of RPC alignment.
 
