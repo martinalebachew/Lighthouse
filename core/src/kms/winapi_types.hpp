3bef3e778f71eb66ce6c6b112b8f0d621bb38e2f
Martin Alebachew <martinalebachew@gmail.com>
2023-05-12 12:51:13 +0300

    Implemented GUID::toString()

--- core/src/kms/winapi_types.hpp
+++ core/src/kms/winapi_types.hpp
@@ -7,6 +7,8 @@ This file defines the WinAPI types required for KMS.
 
 #pragma once
 #include <cstdint>
+#include <cstdio>
+#include <string>
 
 typedef uint16_t WORD;
 typedef uint32_t DWORD;
@@ -21,6 +23,17 @@ struct GUID {
   uint16_t Data2;
   uint16_t Data3;
   byte Data4[8];
+
+  std::string toString() {
+    char guid_cstr[37];
+    snprintf(
+      guid_cstr, sizeof(guid_cstr), "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x", Data1,
+      Data2, Data3, Data4[0], Data4[1], Data4[2], Data4[3], Data4[4], Data4[5], Data4[6],
+      Data4[7]
+    );
+
+    return std::string(guid_cstr);
+  }
 } __attribute__((packed)); // Disabling compiler alignment in favor of RPC alignment
 
 struct FILETIME {
