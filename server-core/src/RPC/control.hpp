0ad8eaf6a289c4e511960e11728d95c3d7795420
Martin Alebachew <martinalebachew@gmail.com>
2023-05-12 13:54:01 +0300

    Added missing directives
    - Added missing preprocessor directives to control.hpp.
    - Reformatted file in human-readable language.

--- server-core/src/RPC/control.hpp
+++ server-core/src/RPC/control.hpp
@@ -6,14 +6,18 @@ This file contains RPC conversation control
 wrapper.
 */
 
+#pragma once
+#include <iostream>
+#include <vector>
+#include <boost/asio.hpp>
 #include "primitives.hpp"
+
 #define HEAD_SIZE                                                              \
   10 // Frag length is specified in 9-10 bytes of CO PDUs (index 8-9)
 
 namespace RPC::Control {
 struct Conversation {
-  std::vector<byte> inline ReceiveFragment(
-      boost::asio::ip::tcp::socket &socket) {
+  std::vector<byte> inline ReceiveFragment(boost::asio::ip::tcp::socket &socket) {
     std::vector<byte> fragment(HEAD_SIZE);
     boost::asio::read(socket, boost::asio::buffer(fragment));
 
@@ -21,29 +25,23 @@ struct Conversation {
     // endianness This only handles little endian to little endian
     size_t size = *(u_int16 *)(&fragment.at(HEAD_SIZE - 2));
     fragment.resize(size);
-    std::cout << "[RPC] <<< PDU Size: " << size << " bytes" << std::endl;
+    std::cout << "[RPC] >>> PDU Size: " << size << " bytes" << std::endl;
 
-    boost::asio::read(
-        socket,
-        boost::asio::buffer(fragment.data() + HEAD_SIZE * sizeof(fragment[0]),
-                            size - HEAD_SIZE)); // Receive rest of the fragment
+    boost::asio::read(socket, boost::asio::buffer(fragment.data() + HEAD_SIZE * sizeof(fragment[0]), size - HEAD_SIZE)); // Receive rest of the fragment
     return fragment;
   };
 
   std::vector<byte> inline ReceivePDU(boost::asio::ip::tcp::socket &socket) {
     // No support for fragmented PDUs
-    return ReceiveFragment(
-        socket); // TODO: Throw error if PDU is fragmented (check flags)
+    return ReceiveFragment(socket); // TODO: Throw error if PDU is fragmented (check flags)
   }
 
-  void inline SendFragment(boost::asio::ip::tcp::socket &socket,
-                           std::vector<byte> fragment) {
-    std::cout << "[RPC] >>> PDU Size: " << fragment.size() << " bytes" << std::endl;
+  void inline SendFragment(boost::asio::ip::tcp::socket &socket, std::vector<byte> fragment) {
+    std::cout << "[RPC] <<< PDU Size: " << fragment.size() << " bytes" << std::endl;
     boost::asio::write(socket, boost::asio::buffer(fragment));
   }
 
-  void inline SendPDU(boost::asio::ip::tcp::socket &socket,
-                      std::vector<byte> pdu) {
+  void inline SendPDU(boost::asio::ip::tcp::socket &socket, std::vector<byte> pdu) {
     // No support for fragmented PDUs
     SendFragment(socket, pdu);
   }
