476107c9f6ddc09db208e6bc12921b803b1e2cbf
Martin Alebachew <martinalebachew@gmail.com>
2023-05-13 21:30:42 +0300

    Created basic prototype
    - Created basic prototype.

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,8 +1,37 @@
 import { createRoot } from "react-dom/client";
 
+function App() {
+  return (
+    <div>
+      <h1>Ligthouse Client</h1>
+
+      <div>
+        <h3>KMS Server IP Address</h3>
+        <input style={{ fontFamily: "Satoshi", display: "inline-block" }} />
+        <button>Save</button>
+      </div>
+
+      <div>
+        <h3>Activation Status</h3>
+        <h4>Activated :D</h4>
+      </div>
+
+      <div>
+        <button>Activate Windows</button>
+      </div>
+
+      <div>
+        <button>Rearm Machine License</button>
+        <button>Change License To GVLK</button>
+        <button>Force Refresh Activation</button>
+      </div>
+    </div>
+  );
+}
+
 function render() {
   const root = createRoot(document.getElementById("root"));
-  root.render(<h2>Lighthouse Client!</h2>);
+  root.render(<App />);
 }
 
 render();
--- client/src/index.ts
+++ client/src/index.ts
@@ -13,8 +13,11 @@ if (require('electron-squirrel-startup')) {
 const createWindow = (): void => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800,
+    height: 500,
+    width: 400,
+    resizable: false,
+    maximizable: false,
+    
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
@@ -24,7 +27,7 @@ const createWindow = (): void => {
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
