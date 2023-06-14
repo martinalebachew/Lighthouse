812bcc63432cf34490544535c846f9501d36f7aa
0xmartin <martinalebachew@gmail.com>
2023-06-14 21:33:47 +0300

    Laid out main functionality in menu strip
    - Laid out main functionality in menu strip.

--- client/Lighthouse.Designer.cs
+++ client/Lighthouse.Designer.cs
@@ -28,21 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip = new MenuStrip();
+            windowsToolStripMenuItem = new ToolStripMenuItem();
+            activateWindowsToolStripMenuItem = new ToolStripMenuItem();
+            viewActivationDetailsToolStripMenuItem = new ToolStripMenuItem();
+            installProductKeyToolStripMenuItem = new ToolStripMenuItem();
+            removeProductKeyToolStripMenuItem = new ToolStripMenuItem();
+            officeToolStripMenuItem = new ToolStripMenuItem();
+            activateVolumeLicenseVersionToolStripMenuItem = new ToolStripMenuItem();
+            locateVolumeLicenseVersionToolStripMenuItem = new ToolStripMenuItem();
+            downloadVolumeLicenseVersionToolStripMenuItem = new ToolStripMenuItem();
+            installVolumeLicenseVersionToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { windowsToolStripMenuItem, officeToolStripMenuItem, aboutToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(692, 24);
+            menuStrip.TabIndex = 2;
+            menuStrip.Text = "menuStrip1";
+            // 
+            // windowsToolStripMenuItem
+            // 
+            windowsToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { activateWindowsToolStripMenuItem, viewActivationDetailsToolStripMenuItem, installProductKeyToolStripMenuItem, removeProductKeyToolStripMenuItem });
+            windowsToolStripMenuItem.Name = "windowsToolStripMenuItem";
+            windowsToolStripMenuItem.Size = new Size(68, 20);
+            windowsToolStripMenuItem.Text = "Windows";
+            // 
+            // activateWindowsToolStripMenuItem
+            // 
+            activateWindowsToolStripMenuItem.Name = "activateWindowsToolStripMenuItem";
+            activateWindowsToolStripMenuItem.Size = new Size(194, 22);
+            activateWindowsToolStripMenuItem.Text = "Activate Windows";
+            // 
+            // viewActivationDetailsToolStripMenuItem
+            // 
+            viewActivationDetailsToolStripMenuItem.Name = "viewActivationDetailsToolStripMenuItem";
+            viewActivationDetailsToolStripMenuItem.Size = new Size(194, 22);
+            viewActivationDetailsToolStripMenuItem.Text = "View Activation Details";
+            // 
+            // installProductKeyToolStripMenuItem
+            // 
+            installProductKeyToolStripMenuItem.Name = "installProductKeyToolStripMenuItem";
+            installProductKeyToolStripMenuItem.Size = new Size(194, 22);
+            installProductKeyToolStripMenuItem.Text = "Install Product Key";
+            // 
+            // removeProductKeyToolStripMenuItem
+            // 
+            removeProductKeyToolStripMenuItem.Name = "removeProductKeyToolStripMenuItem";
+            removeProductKeyToolStripMenuItem.Size = new Size(194, 22);
+            removeProductKeyToolStripMenuItem.Text = "Remove Product Key";
+            // 
+            // officeToolStripMenuItem
+            // 
+            officeToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { activateVolumeLicenseVersionToolStripMenuItem, locateVolumeLicenseVersionToolStripMenuItem, downloadVolumeLicenseVersionToolStripMenuItem, installVolumeLicenseVersionToolStripMenuItem });
+            officeToolStripMenuItem.Name = "officeToolStripMenuItem";
+            officeToolStripMenuItem.Size = new Size(51, 20);
+            officeToolStripMenuItem.Text = "Office";
+            // 
+            // activateVolumeLicenseVersionToolStripMenuItem
+            // 
+            activateVolumeLicenseVersionToolStripMenuItem.Name = "activateVolumeLicenseVersionToolStripMenuItem";
+            activateVolumeLicenseVersionToolStripMenuItem.Size = new Size(254, 22);
+            activateVolumeLicenseVersionToolStripMenuItem.Text = "Activate Volume License Version";
+            // 
+            // locateVolumeLicenseVersionToolStripMenuItem
+            // 
+            locateVolumeLicenseVersionToolStripMenuItem.Name = "locateVolumeLicenseVersionToolStripMenuItem";
+            locateVolumeLicenseVersionToolStripMenuItem.Size = new Size(254, 22);
+            locateVolumeLicenseVersionToolStripMenuItem.Text = "Locate Volume License Version";
+            // 
+            // downloadVolumeLicenseVersionToolStripMenuItem
+            // 
+            downloadVolumeLicenseVersionToolStripMenuItem.Name = "downloadVolumeLicenseVersionToolStripMenuItem";
+            downloadVolumeLicenseVersionToolStripMenuItem.Size = new Size(254, 22);
+            downloadVolumeLicenseVersionToolStripMenuItem.Text = "Download Volume License Version";
+            // 
+            // installVolumeLicenseVersionToolStripMenuItem
+            // 
+            installVolumeLicenseVersionToolStripMenuItem.Name = "installVolumeLicenseVersionToolStripMenuItem";
+            installVolumeLicenseVersionToolStripMenuItem.Size = new Size(254, 22);
+            installVolumeLicenseVersionToolStripMenuItem.Text = "Install Volume License Version";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(52, 20);
+            aboutToolStripMenuItem.Text = "About";
             // 
             // Lighthouse
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(692, 472);
+            Controls.Add(menuStrip);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MainMenuStrip = menuStrip;
+            MaximizeBox = false;
+            MinimizeBox = false;
             Name = "Lighthouse";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Lighthouse Client For Windows";
+            Text = "Lighthouse Client";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem windowsToolStripMenuItem;
+        private ToolStripMenuItem officeToolStripMenuItem;
+        private ToolStripMenuItem viewActivationDetailsToolStripMenuItem;
+        private ToolStripMenuItem installProductKeyToolStripMenuItem;
+        private ToolStripMenuItem removeProductKeyToolStripMenuItem;
+        private ToolStripMenuItem locateVolumeLicenseVersionToolStripMenuItem;
+        private ToolStripMenuItem downloadVolumeLicenseVersionToolStripMenuItem;
+        private ToolStripMenuItem installVolumeLicenseVersionToolStripMenuItem;
+        private ToolStripMenuItem activateVolumeLicenseVersionToolStripMenuItem;
+        private ToolStripMenuItem activateWindowsToolStripMenuItem;
     }
 }
