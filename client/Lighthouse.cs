e634135d390fb880ddb44dd15009fa3d93f1fc3f
0xmartin <martinalebachew@gmail.com>
2023-06-17 13:09:45 +0300

    Parsed Windows activation details
    - Created class Windows.ActivationDetails.
    - Added slmgr stdout parsing functionality.

--- client/Lighthouse.cs
+++ client/Lighthouse.cs
@@ -9,7 +9,18 @@ namespace client
 
         private void viewActivationDetailsToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(Utilities.Windows.getActivationStatus(), "Windows Activation Details", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Utilities.Windows.ActivationDetails activationDetails = Utilities.Windows.getActivationDetails();
+            string formattedActivationDetails = $"Volume Licensed: {activationDetails.isVolumeLicense}\n" +
+            $"Volume Expiration: {activationDetails.volumeExpiration}\n" +
+            $"License Status: {activationDetails.licenseStatus}\n" +
+            $"Client Machine ID: {activationDetails.clientMachineID}\n" +
+            $"Activation Interval: {activationDetails.activationInterval}\n" +
+            $"Renewal Interval: {activationDetails.renewalInterval}\n" +
+            $"KMS Host Machine Registered: {activationDetails.kmsHostMachineRegistered}\n" +
+            $"KMS Host Machine Activated: {activationDetails.kmsHostMachineActivated}\n" +
+            $"KMS Host Machine EPID: {activationDetails.kmsHostMachineEPID}\n";
+
+            MessageBox.Show(formattedActivationDetails, "Windows Activation Details", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
--- client/Utilities/Windows.cs
+++ client/Utilities/Windows.cs
@@ -8,10 +8,49 @@ namespace client.Utilities
 {
     internal static class Windows
     {
-        private static readonly string slgmr = Environment.GetEnvironmentVariable("windir") + "\\System32\\slmgr.vbs ";
-        public static string getActivationStatus()
+        private static readonly string scriptPath = Environment.GetEnvironmentVariable("windir") + "\\System32\\slmgr.vbs ";
+
+        public class ActivationDetails
         {
-            return Run.CScript(slgmr + "/dli");
+            public bool isVolumeLicense;
+            public string volumeExpiration;
+            public string licenseStatus;
+            public string clientMachineID;
+            public string activationInterval;
+            public string renewalInterval;
+            public string kmsHostMachineRegistered;
+            public string kmsHostMachineActivated;
+            public string kmsHostMachineEPID;
+
+            private static string ExtractFromDict(Dictionary<string, string> dict, string key, string defaultValue = "")
+            {
+                return dict.ContainsKey(key) ? dict[key] : defaultValue;
+            }
+
+            public ActivationDetails(Dictionary<string, string> rawDetails)
+            {
+                isVolumeLicense = ExtractFromDict(rawDetails, "Description").Contains("VOLUME_KMSCLIENT");
+                volumeExpiration = ExtractFromDict(rawDetails, "Volume activation expiration");
+                licenseStatus = ExtractFromDict(rawDetails, "License Status");
+                clientMachineID = ExtractFromDict(rawDetails, "Client Machine ID (CMID)");
+                activationInterval = ExtractFromDict(rawDetails, "Activation interval");
+                renewalInterval = ExtractFromDict(rawDetails, "Renewal interval");
+                kmsHostMachineRegistered = ExtractFromDict(rawDetails, "Registered KMS machine name");
+                kmsHostMachineActivated = ExtractFromDict(rawDetails, "KMS machine IP address");
+                kmsHostMachineEPID = ExtractFromDict(rawDetails, "KMS machine extended PID");
+            }
+        }
+
+        public static ActivationDetails getActivationDetails()
+        {
+            Dictionary<string, string> rawDetails = new Dictionary<string, string>();
+            foreach (string line in Run.CScript(scriptPath + "/dli").Split("\r\n"))
+            {
+                string[] pair = line.Split(": ");
+                if (pair.Length == 2) rawDetails[pair[0].Trim()] = pair[1];
+            }
+
+            return new ActivationDetails(rawDetails);
         }
     }
 }
