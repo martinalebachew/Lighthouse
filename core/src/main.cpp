385ebb7cee438efca17a470de743e6934570fda0
Martin Alebachew <martinalebachew@gmail.com>
2023-10-18 10:39:51 +0300

    Created main file

---
+++ core/src/main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+// (C) Martin Alebachew, 2023
+
+#include <iostream>
+
+int main(int argc, const char* argv[]) {
+  std::cout << "Lighthouse Core" << std::endl;
+  std::cout << "(C) Martin Alebachew, 2023\n" << std::endl;
+
+  std::cin.get();
+  return 0;
+}
