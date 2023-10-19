4cd864989427a65a90f474bea30347fef246d745
Martin Alebachew <martinalebachew@gmail.com>
2023-10-19 23:40:36 +0300

    Merged presentation types into bind.hpp
    - Moved presentation.hpp declarations into bind.hpp, as they are only used by the Bind PDU.

--- core/src/rpc/pdu/bind.hpp
+++ core/src/rpc/pdu/bind.hpp
@@ -6,12 +6,34 @@ This file defines the RPC Bind PDU struct.
 */
 
 #pragma once
-#include "../presentation.hpp"
 #include "../primitives.hpp"
+#include "../uuid.hpp"
 #include "types.hpp"
 #include <cstdlib>
 #include <cstring>
 #include <vector>
+
+typedef u_int16 p_context_id_t;
+
+struct p_syntax_id_t {
+  dce_uuid_t if_uuid;
+  u_int32 if_version;
+} __attribute__((packed)); // Disabling compiler alignment in favor of RPC alignment
+
+struct p_cont_elem_t {
+  p_context_id_t p_cont_id;
+  u_int8 n_transfer_syn; // Number of items
+  u_int8 reserved;
+  p_syntax_id_t abstract_syntax;
+  p_syntax_id_t* transfer_syntaxes; // Heap-allocated array
+} __attribute__((packed));          // Disabling compiler alignment in favor of RPC alignment
+
+struct p_cont_list_t {
+  u_int8 n_context_elem; // Number of items
+  u_int8 reserved;
+  u_int16 reserved2;
+  p_cont_elem_t* p_cont_elem; // Heap-allocated array
+} __attribute__((packed));    // Disabling compiler alignment in favor of RPC alignment
 
 namespace RPC::PDU {
 struct Bind {
