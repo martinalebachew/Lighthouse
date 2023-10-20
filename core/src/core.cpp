27908e69a8d19c4e8e7e05671a9b283dbb7e3f26
Martin Alebachew <martinalebachew@gmail.com>
2023-10-20 20:07:15 +0300

    Split main program
    - Split main program into header and code files.
    - Added descriptions to both files.

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -1,20 +1,11 @@
 // core.cpp
 // (C) Martin Alebachew, 2023
 
-#include "kms/request.hpp"
-#include "kms/response.hpp"
-#include "rpc/control.hpp"
-#include "rpc/pdu/bind.hpp"
-#include "rpc/pdu/bind_ack.hpp"
-#include "rpc/pdu/request.hpp"
-#include "rpc/pdu/response.hpp"
-#include <boost/asio.hpp>
-#include <iostream>
+/*
+This file implements the enrty point for Lighthouse KMS server.
+*/
 
-using boost::asio::ip::tcp;
-using namespace RPC;
-
-constexpr int port = 1688; // Default KMS port
+#include "core.hpp"
 
 int main(int argc, const char* argv[]) {
   std::cout
---
+++ core/src/core.hpp
@@ -0,0 +1,24 @@
+// core.hpp
+// (C) Martin Alebachew, 2023
+
+/*
+This file defines the enrty point for Lighthouse KMS server.
+*/
+
+#pragma once
+#include "kms/request.hpp"
+#include "kms/response.hpp"
+#include "rpc/control.hpp"
+#include "rpc/pdu/bind.hpp"
+#include "rpc/pdu/bind_ack.hpp"
+#include "rpc/pdu/request.hpp"
+#include "rpc/pdu/response.hpp"
+#include <boost/asio.hpp>
+#include <iostream>
+
+using boost::asio::ip::tcp;
+using namespace RPC;
+
+constexpr int port = 1688; // Default KMS port
+
+int main(int argc, const char* argv[]);
