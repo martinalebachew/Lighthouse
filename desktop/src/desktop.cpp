fdc37e0f5c164b327eb2a85e0a6b0521b2a39c94
Martin Alebachew <martinalebachew@gmail.com>
2023-10-23 14:11:09 +0300

    Created webapp wrapper
    - Created webapp wrapper based on Ultralight. This is a heavily modified template from ultralight-quick-start.
    - Adjusted CMake configuration, mainly set entry point to webapp build directory and limited support to Windows only.

---
+++ desktop/src/desktop.cpp
@@ -0,0 +1,15 @@
+// desktop.cpp
+// (C) Martin Alebachew, 2023
+
+/*
+This file implements the entry point for Lighthouse Desktop GUI.
+*/
+
+#include "gui/application.hpp"
+
+int main() {
+  Application GUI;
+  GUI.Run();
+
+  return 0;
+}
